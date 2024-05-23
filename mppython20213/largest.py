0f3c053a4952516d860e6fe3327bbc277b7ecf5b
Naveenmaddy7081 <85430074+Naveenmaddy7081@users.noreply.github.com>
2024-05-23 21:53:22 +0530

    Add files via upload

---
+++ mppython20213/largest.py
@@ -0,0 +1,33 @@
+# n1=int (input("enter the number"))
+# n2=int (input("enter the number"))
+# n3=int(input("enter the number"))
+# def largest():
+#     if(n1>n2):
+#         if(n1>n3):
+#             print("n1 the largest number")
+#         else:
+#             print("n3 is largest number")
+#     elif(n2>n3):
+#         print("n2 is largest number")
+#     else:
+#         print("n3 is largest number")
+#
+# largest()
+#
+# x =input("enter the string")
+# y=x[-1::-1]
+# if(x==y):
+#     print("this is palindrome")
+# else:
+#     print("this is not palindrome")
+#
+#
+# def myReverse():
+#     x=input("enter the string")
+#     return x[::-1]
+# str = myReverse()
+# print(str)
+
+x=float(input("enter the temperature"))
+fahrenheit=(x*1.8)+32
+print("celsius to fahrenheit",fahrenheit)
